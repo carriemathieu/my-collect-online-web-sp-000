7f94bb939c2bec8bb6af269238708cb2c208e5a9
Carrie Mathieu <carrieburks@gmail.com>
2020-08-09 20:18:00 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,3 +1,15 @@
 def my_collect(collection)
+  i = 0
+  array = []
+  while i < collection.length
+    array << yield(collection[i])
+    i += 1
+  end
+  array
 end
 
+def hello(array)
+  
+end
+
+hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
